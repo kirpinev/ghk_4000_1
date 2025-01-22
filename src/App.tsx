6e5a97c6162e86e9a281388e40068f2c3e2ad12c
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-01-22 14:03:10 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export const App = () => {
     setLoading(true);
 
     Promise.resolve().then(() => {
-      window.gtag("event", "4000_processing_click", {
+      window.gtag("event", "4000_processing_click_var1", {
         variant_name: "ghk_4000_1",
       });
       setLoading(false);
